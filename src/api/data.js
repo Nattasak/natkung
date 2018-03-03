6c34470c22c0fbd9f2b97d781a20a1cd51db2473
Nattasak Lertpoompunya <dottedes@gmail.com>
2018-03-03 22:36:42 +0700

    :clown_face: api data

---
+++ src/api/data.js
@@ -0,0 +1,32 @@
+export const projectListJSON = [
+  {
+    id: 1,
+    title: 'ชมรมกัลยาณธรรม - Dhamma books',
+    desc: 'รวบรวมหนังสือธรรมะดีๆ จากชมรมกัลยาณธรรม',
+    href: 'http://nattasak.github.io/kanlayanatam-books',
+  },
+  {
+    id: 2,
+    title: 'Dev NEED to Know',
+    desc: 'รวบรวมความรู้ดีๆ สำหรับ dev.',
+    href: 'http://nattasak.github.io/dev-need-to-know',
+  },
+  {
+    id: 3,
+    title: 'Anime Splash',
+    desc: 'Angular 5 + Bootstrap + HttpClient',
+    href: 'http://nattasak.github.io/anime-splash',
+  },
+  {
+    id: 4,
+    title: 'Bulma Axios',
+    desc: 'React 16 + React router + Bulma + Axios',
+    href: 'http://nattasak.github.io/bulma-axios',
+  },
+  {
+    id: 5,
+    title: 'Todo Winter',
+    desc: 'React 16 + Redux + LocalStorage',
+    href: 'http://nattasak.github.io/job-quest-winter-2017',
+  },
+]
