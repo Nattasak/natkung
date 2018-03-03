b93bb866f605f7e99fc9ee7ee94577e1134e497b
Nattasak Lertpoompunya <dottedes@gmail.com>
2018-03-04 03:57:57 +0700

    :fire: Remove old projects

--- src/api/data.js
+++ src/api/data.js
@@ -11,22 +11,4 @@ export const projectListJSON = [
     desc: 'รวบรวมความรู้ดีๆ สำหรับ dev.',
     href: 'http://nattasak.github.io/dev-need-to-know',
   },
-  {
-    id: 3,
-    title: 'Anime Splash',
-    desc: 'Angular 5 + Bootstrap + HttpClient',
-    href: 'http://nattasak.github.io/anime-splash',
-  },
-  {
-    id: 4,
-    title: 'Bulma Axios',
-    desc: 'React 16 + React router + Bulma + Axios',
-    href: 'http://nattasak.github.io/bulma-axios',
-  },
-  {
-    id: 5,
-    title: 'Todo Winter',
-    desc: 'React 16 + Redux + LocalStorage',
-    href: 'http://nattasak.github.io/job-quest-winter-2017',
-  },
 ]
