005540d33434d44bbe8c9df5839282686a342cf1
Nattasak Lertpoompunya <dottedes@gmail.com>
2018-03-14 09:26:56 +0700

    :lipstick: Try css variables

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,27 +4,33 @@ import styled, { injectGlobal } from 'styled-components'
 import Header from './Header'
 
 injectGlobal`
+  :root {
+    --main-font-family: 'Open Sans', sans-serif;
+    --main-font-size: 17px;
+    --main-link-color: #0366d6;
+    --main-link-hover-color: #024ea4;
+  }
   body {
     counter-reset: item; /* Counters */
   }
 `
 
 const PageContainer = styled.div`
-  font-family: 'Open Sans', sans-serif;
-  font-size: 17px;
+  font-family: var(--main-font-family);
+  font-size: var(--main-font-size);
 `
 
 const ChildrenWrapper = styled.main`
   padding: 2rem 1.5rem;
 
   a {
-    color: #0366d6;
+    color: var(--main-link-color);
     font-size: 14px;
     font-weight: 600;
     text-decoration: none;
 
     &:hover {
-      color: #024ea4;
+      color: var(--main-link-hover-color);
     }
   }
 `
